50d98165858571ca1502fe536fffab69cc969e49
Leonardo Pompili <120735073+shenron221B@users.noreply.github.com>
2026-05-20 17:48:49 +0200

    Delete src/serverledge directory

--- src/serverledge/used_functions/mobilenet-ssd/test.sh
+++
@@ -1,5 +0,0 @@
-
-CLI=$HOME/Dev/serverledge/bin/serverledge-cli
-
-$CLI create -u -f mobilenetFunc --memory 900 --runtime custom --custom_image grussorusso/mobilenetssd
-$CLI invoke -f mobilenetFunc --params_file input.json --ret_output
